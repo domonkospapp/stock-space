1b5a0be6b449133b48e44647cf2e487d2bcd98f5
Domonkos Papp <pappdomonkos96@gmail.com>
2025-09-24 20:08:37 +0200

    More width for the site

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -83,7 +83,7 @@ export default function Portfolio() {
 
   return (
     <div className="min-h-screen p-8" style={{ backgroundColor: "#292929" }}>
-      <div className="max-w-6xl mx-auto">
+      <div className="max-w-7xl mx-auto">
         <div className="flex items-start justify-between mb-8">
           <PortfolioNavigation />
           <PortfolioHeader
